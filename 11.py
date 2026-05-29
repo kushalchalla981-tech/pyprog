2c0ffb240f85f451888f9c84a804c7993d28ed2c
kushal <kushalchalla981@gmail.com>
2026-05-29 23:29:49 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,9 @@
+name = input("Enter Employee Name: ")
+basic_salary = float(input("Enter Basic Salary: "))
+bonus = basic_salary * 0.10
+total_salary = basic_salary + bonus
+print("\nEmployee Salary Details")
+print("Employee Name :", name)
+print("Basic Salary :", basic_salary)
+print("Bonus (10%) :", bonus)
+print("Total Salary :", total_salary)
---
+++ 9.py
@@ -0,0 +1,5 @@
+dict1 = {'a': 10, 'b': 20, 'c': 30}
+dict2 = {'d': 40, 'e': 50, 'f': 60}
+dict1.update(dict2)
+print("Merged Dictionary:")
+print(dict1)
